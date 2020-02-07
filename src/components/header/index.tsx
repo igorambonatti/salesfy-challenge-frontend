535ff292a207816b4c77a3a40e05cf53ba18ae1c
Igor ambonatti <contatoambonatti@gmail.com>
2020-02-07 18:36:23 -0300

    Organizando estrutura

---
+++ src/components/header/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Container, Nav, User } from "./styles";
+
+const Header: React.FC = () => {
+  return (
+    <Container>
+      <Nav></Nav>
+      <User></User>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/header/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+export const Nav = styled.div``;
+export const User = styled.div``;
